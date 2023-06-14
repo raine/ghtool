082831b15ea0bf86c2271873e4a9763bccf416de
Raine Virta <raine.virta@gmail.com>
2023-06-15 00:00:33 +0300

    update build script

--- ghtool/build.rs
+++ ghtool/build.rs
@@ -1,6 +1,6 @@
 fn main() {
     cynic_codegen::register_schema("github")
-        .from_sdl_file("../github_schema/github.graphql")
+        .from_sdl_file("./github.graphql")
         .unwrap()
         .as_default()
         .unwrap();
