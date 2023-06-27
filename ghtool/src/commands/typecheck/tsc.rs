fdb6167a54278fa2fa8e2bd6cd29b71e189f370b
Raine Virta <raine.virta@gmail.com>
2023-06-27 19:36:30 +0300

    tsc: improve log parsing

--- ghtool/src/commands/typecheck/tsc.rs
+++ ghtool/src/commands/typecheck/tsc.rs
@@ -5,7 +5,10 @@ const TIMESTAMP_PATTERN: &str = r"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d+Z";
 const ANSI_RESET: &str = r"\u{1b}\[0m";
 
 lazy_static! {
+    /// Regex to match a timestamp and single space after it
     static ref TIMESTAMP: Regex = Regex::new(&format!(r"{}\s", TIMESTAMP_PATTERN)).unwrap();
+
+    /// Regex to match an error line of the TypeScript compiler (tsc) log
     static ref TSC_ERROR_LINE: Regex = Regex::new(&format!(
         r"(?i){TIMESTAMP_PATTERN}\s+(?P<error>##\[error\]).*?({ANSI_RESET})?(?P<path>[a-zA-Z0-9._/-]*)\(\d+,\d+\):\serror\sTS\d+",
         //                                                ^^^^^^^^^^^^^^^^^^ See test_extract_failing_files_3
@@ -32,6 +35,7 @@ pub struct TscLogParser {
     current_error: Option<TscError>,
     all_errors: Vec<TscError>,
     error_tag_start_col: usize,
+    error_line_count: usize,
 }
 
 impl TscLogParser {
@@ -41,6 +45,7 @@ impl TscLogParser {
             current_error: None,
             all_errors: Vec::new(),
             error_tag_start_col: 0,
+            error_line_count: 0,
         }
     }
 
@@ -61,10 +66,10 @@ impl TscLogParser {
                 }
             }
             State::ParsingError => {
+                self.error_line_count += 1;
+
                 if TSC_ERROR_LINE.is_match(full_line) {
-                    let current_error = std::mem::take(&mut self.current_error);
-                    self.all_errors.push(current_error.unwrap());
-                    self.state = State::LookingForError;
+                    self.reset_to_looking_for_errors();
                     self.parse_line(full_line)?;
                 } else if full_line.chars().nth(self.error_tag_start_col) == Some(' ') {
                     // ##[error]src/index.ts(3,21): error TS2769: No overload matches this call.
@@ -75,10 +80,25 @@ impl TscLogParser {
                         .unwrap()
                         .lines
                         .push(line.to_string());
+                } else if self.error_line_count == 1 {
+                    // The first line after seeing an error should either:
+                    // a) be a new error (first if condition)
+                    // b) be indented which means it's part of the error (second else if condition)
+                    // In any other case it would be some unrelated output and we want to get back
+                    // to looking for errors. See test_extract_failing_files_4.
+                    self.reset_to_looking_for_errors()
                 }
             }
         }
         Ok(())
+    }
+
+    fn reset_to_looking_for_errors(&mut self) {
+        let current_error = std::mem::take(&mut self.current_error);
+        self.all_errors.push(current_error.unwrap());
+        self.state = State::LookingForError;
+        self.error_tag_start_col = 0;
+        self.error_line_count = 0;
     }
 
     pub fn parse(log: &str) -> Result<Vec<TscError>, eyre::Error> {
@@ -174,6 +194,30 @@ mod tests {
                 path: "src/index.ts".to_string(),
                 lines: vec![
                     "\u{1b}[32m@owner/package:typecheck: \u{1b}[0msrc/index.ts(63,7): error TS1117: An object literal cannot have multiple properties with the same name.".to_string()
+                ],
+            },
+        ]);
+    }
+
+    #[test]
+    fn test_extract_failing_files_4() {
+        let logs = r#"
+2023-06-27T08:32:59.2543883Z ##[error][34m@project:typecheck: [0msrc/components/Component.spec.tsx(58,8): error TS2739: Type '{ foo: string; }' is missing the following properties from type 'Props': bar
+2023-06-27T08:33:50.2166735Z [34m@project:typecheck: [0m[41m[30m ELIFECYCLE [39m[49m [31mCommand failed with exit code 1.[39m
+2023-06-27T08:33:50.2437013Z [91m[34m@project:typecheck: [0mERROR: command finished with error: command (/home/runner) pnpm run typecheck exited (1)[0m
+2023-06-27T08:33:50.3894539Z [34mproject:typecheck: [0m[41m[30m ELIFECYCLE [39m[49m [31mCommand failed.[39m
+2023-06-27T08:33:50.3968735Z [91mcommand (/home/runner) pnpm run typecheck exited (1)[0m
+2023-06-27T08:33:50.3983800Z 
+2023-06-27T08:33:50.3984922Z  Tasks:    145 successful, 147 total
+2023-06-27T08:33:50.3985487Z Cached:    73 cached, 147 total
+2023-06-27T08:33:50.3985812Z   Time:    3m7.006s"#;
+
+        let failing_files = TscLogParser::parse(logs).unwrap();
+        assert_eq!(failing_files, vec![
+            TscError {
+                path: "src/components/Component.spec.tsx".to_string(),
+                lines: vec![
+                    "\u{1b}[34m@project:typecheck: \u{1b}[0msrc/components/Component.spec.tsx(58,8): error TS2739: Type '{ foo: string; }' is missing the following properties from type 'Props': bar".to_string(),
                 ],
             },
         ]);
